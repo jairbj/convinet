db1c070f4fe3a15ea7bc5688d0b55ba22333004c
Jair Junior <1747830+jairbj@users.noreply.github.com>
2018-10-04 23:32:53 -0300

    Configura a tela de login.
    Configura a tela de login e troca as views do devise por views i18n.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :telefones
   devise_for :usuarios
   get 'paginas/home'
+
+  devise_scope :usuario do
+    root :to => 'devise/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
